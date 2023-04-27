ce0d98edf37b51dd8cbdeb6cd8d6157681c57ee0
acuifex <46835803+acuifex@users.noreply.github.com>
2023-04-27 23:38:45 +0300

    i swear all of those commits will be wiped out

--- sp/src/game/server/physics_main.cpp
+++ sp/src/game/server/physics_main.cpp
@@ -7,7 +7,7 @@
 
 
 #include "cbase.h"
-#ifdef _WIN32 && _MSC_VER != 1929
+#if defined(_WIN32) && _MSC_VER < 1900
 #include "typeinfo.h"
 // BUGBUG: typeinfo stomps some of the warning settings (in yvals.h)
 #pragma warning(disable:4244)
